178aaf22480580a3941a27f73b74351d4011c457
CarlosHel <carlos.d.silva@eletrobras.com>
2025-01-06 22:58:14 -0300

    First Alteraation

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -20,7 +20,7 @@ bool exibirMenu = true;
 while (exibirMenu)
 {
     Console.Clear();
-    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("Escolha uma opção:");
     Console.WriteLine("1 - Cadastrar veículo");
     Console.WriteLine("2 - Remover veículo");
     Console.WriteLine("3 - Listar veículos");
